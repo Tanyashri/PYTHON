3426d919d5f881bfdf8f391c2d709711992a9bd8
Tanyashri <tanyashrim2005@gmail.com>
2024-09-28 21:43:05 +0530

    Carsar Cipher

---
+++ caesar.py
@@ -0,0 +1,14 @@
+alphabets = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+direction=input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
+text=input("Type your message:\n").lower()
+shift=int(input("Type the shift number:\n"))
+    
+def caesar(original_text,shift_amount,encode_or_decode):
+    for letter in original_text:
+        if encode_or_decode=="decode":
+            shift_amount*=-1
+        shifted_position=alphabets.index(letter) - shift_amount
+        shifted_position%=len(alphabets)
+        output_text+=alphabets[shifted_position]
+    print(f"Here is the {encode_or_decode}d result: {output_text}")
+caesar(original_text=text,shift_amount=shift),encode_or
